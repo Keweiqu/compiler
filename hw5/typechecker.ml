77aa477a7e462e23c5ad74a498e05645b4897e61
Kewei Qu <qkw1221@gmail.com>
2017-04-08 14:38:46 -0400

    complete typecheck_stmt typecheck_exp and fixed a bug in create_struct_ctxt

--- hw5/typechecker.ml
+++ hw5/typechecker.ml
@@ -73,25 +73,96 @@ let rec typecheck_exp (c : Tctxt.t) (e : Ast.exp node) : Ast.ty =
   | CBool b -> TBool
   | CInt n -> TInt
   | CStr s -> TRef RString
-  | CArr (ty, exps) -> failwith "unimplemented"
-  | CStruct (id, cfields) -> failwith "unimplemented"
-  | Proj (exp, id) -> failwith "unimplemented"
-  | NewArr (t, exp) -> failwith "unimplemented"
-  | Id id -> failwith "unimplemented"
-  | Index (exp1, exp2) -> failwith "unimplemented"
-  | Call (exp, exps) -> failwith "unimplemented"
+  | CArr (ty, exps) -> 
+    List.iter (fun exp -> if typecheck_exp c exp <> ty then type_error e "typecheck_exp: Carr unmatched types" else ()) exps;
+    TRef (RArray ty)
+  | CStruct (id, cfields) -> 
+    let f = lookup_struct_option id c in
+      begin match f with
+      | None -> type_error e "typecheck_exp: undefined struct symbol"
+      | Some fields ->
+        let sorted_fields = List.sort (fun a b -> String.compare a.fname b.fname) fields in
+        let sorted_cfields = List.sort (fun a b -> String.compare a.cfname b.cfname) cfields in
+        if List.length sorted_fields <> List.length
+ sorted_cfields then
+          type_error e "typecheck_exp: unmatched struct fields"
+        else
+          List.iter2 (fun a b -> 
+                       if a.fname <> b.cfname then
+                         type_error e "typecheck_exp: struct does not have this field"
+                       else
+                         if (typecheck_exp c b.cfinit) <> a.ftyp then
+                           type_error e "typecheck_exp: struct field type unmatched"
+                         else
+                           ()
+                     ) sorted_fields sorted_cfields;
+        TRef (RStruct id)
+      end
+  | Proj (exp, id) -> 
+     begin match typecheck_exp c exp with
+     | TRef (RStruct s) -> 
+        let struct_type = lookup_struct_option s c in
+        begin match struct_type with
+        | None -> type_error e "typecheck_exp: undefined struct type"
+        | Some fields -> try (List.find 
+                                (fun field -> if field.fname = id then true else false) fields).ftyp
+                         with Not_found -> type_error e "typecheck_exp: undefined field name"
+        end
+     | _ -> type_error e "typecheck_exp: projection of a non-struct type"
+     end
+  | NewArr (t, exp) -> 
+     if typecheck_exp c exp <> TInt then
+       type_error e "typecheck_exp: new arr exp not of TInt"
+     else
+       TRef (RArray t)
+  | Id id -> 
+     begin match lookup_option id c with
+     | None -> 
+        begin match lookup_function_option id c with
+        | None -> type_error e "typecheck_exp: undeclared symbol"
+        | Some fty -> TRef (RFun fty)
+        end
+     | Some x -> x
+     end
+  | Index (exp1, exp2) -> 
+     if typecheck_exp c exp2 <> TInt then
+       type_error e "typecheck_exp: index not of type int"
+     else
+       begin match typecheck_exp c exp1 with
+       | TRef (RArray ty) -> ty
+       | _ -> type_error e "typecheck_exp: index into a non-array type"
+       end
+  | Call (exp, exps) -> 
+     let call_ty = typecheck_exp c exp in
+     begin match call_ty with
+     | TRef (RFun fty) ->
+        let ty_list, ret_ty = fty in
+        if List.length ty_list <> List.length exps then
+          type_error e "typecheck_exp: call number of args unmatched"
+        else
+          List.iter2 (fun a b -> 
+                       if a <> typecheck_exp c b then
+                         type_error e "typecheck_exp: call number of args unmatched"
+                       else
+                         ()) ty_list exps;
+        begin match ret_ty with
+        | RetVoid -> type_error e "typecheck_exp: call to a void return function"
+        | RetVal t -> t
+        end
+     | _ -> type_error e "typecheck_exp: call to not a function"
+     end
   | Bop (op, exp1, exp2) -> 
     let t1, t2, tret = typ_of_binop op in
-      if (typecheck_exp c exp1) != t1 then
+      if (typecheck_exp c exp1) <> t1 then
         type_error exp1 "typecheck_exp: invalid type for binop exp1"
       else
-        if (typecheck_exp c exp2) != t2 then
+        if (typecheck_exp c exp2) <> t2 then
           type_error exp2 "typecheck_exp: invalid type for binop exp2"
         else
           tret
   | Uop (op, exp) ->
     let t, tret = typ_of_unop op in
-      if (typecheck_exp c exp) != t then
+      if (typecheck_exp c exp) <> t then
         type_error exp "typecheck_exp: invalid type for unop exp"
       else
         tret
@@ -125,12 +196,16 @@ let rec typecheck_stmt (tc : Tctxt.t) (s:Ast.stmt node) (to_ret:ret_ty) : Tctxt.
   | Assn (lhs, rhs) ->
     let lhs_ty = typecheck_exp tc lhs in
     let rhs_ty = typecheck_exp tc rhs in
-      if lhs_ty != rhs_ty then
+      if lhs_ty <> rhs_ty then
         type_error s "typecheck_stmt: assignment lhs and rhs do not match"
       else
         (tc, NoReturn)
   | Decl (id, exp) -> 
-    let t = typecheck_exp tc exp in (add_local tc id t, NoReturn)
+     let id_ty = lookup_option id tc in
+     if id_ty <> None && id_ty <> Some (typecheck_exp tc exp) then
+       type_error s "typecheck_stmt: overwrite variable declaration"
+     else
+       let t = typecheck_exp tc exp in (add_local tc id t, NoReturn)
   | Ret exp -> 
     begin match exp with
     | None -> 
@@ -153,10 +228,15 @@ let rec typecheck_stmt (tc : Tctxt.t) (s:Ast.stmt node) (to_ret:ret_ty) : Tctxt.
         | None -> type_error s "typecheck_stmt: undefined function"
         | Some (ty_list, ret_ty) -> 
           if ret_ty = RetVoid then
-            let _ = List.iter2 (fun exp expected_rt -> 
-              if (typecheck_exp tc exp) <> expected_rt then
-               type_error s "typecheck_stmt: function argument type not match" else ()) exp_list ty_list in
-            (tc, NoReturn)
+            if List.length exp_list <> List.length ty_list then
+              type_error s "typecheck_stmt: unmatched number of function args"
+            else
+              let _ = List.iter2 (fun exp expected_rt -> 
+                                   if (typecheck_exp tc exp) <> expected_rt then
+                                     type_error s "typecheck_stmt: function argument type not match"
+                                   else ()
+                                 ) exp_list ty_list in
+              (tc, NoReturn)
           else 
             type_error s "typecheck_stmt: scall function not return void"
         end
@@ -173,7 +253,31 @@ let rec typecheck_stmt (tc : Tctxt.t) (s:Ast.stmt node) (to_ret:ret_ty) : Tctxt.
             (tc, Return)
           else 
             (tc, NoReturn)
-  | _ -> failwith "typecheck_stmt unimplemented"
+  | For (vdecl_list, None, None, b) ->
+    let vdecls = List.map (fun a -> no_loc (Decl a)) vdecl_list in
+      let ctxt2 = List.fold_left (fun acc stmt -> fst (typecheck_stmt acc stmt to_ret)) tc vdecls in
+        let _ = typecheck_block b ctxt2 to_ret NoReturn in
+    (tc, NoReturn)
+  | For (vdecl_list, Some exp, Some stmt, b) ->
+     let vdecls = List.map (fun a -> no_loc (Decl a)) vdecl_list in
+     let ctxt2 = List.fold_left (fun acc stmt -> fst (typecheck_stmt acc stmt to_ret)) tc vdecls in
+       let exp_ty = typecheck_exp ctxt2 exp in
+         if exp_ty <> TBool then 
+           type_error s "typecheck_stmt: for loop exp not a boolean"
+         else
+           let _, stmt_ty = typecheck_stmt ctxt2 stmt to_ret in
+           if stmt_ty <> NoReturn then
+             type_error s "typecheck_stmt: for loop stmt must be NoReturn"
+           else 
+             let _ = typecheck_block b ctxt2 to_ret NoReturn in
+             (tc, NoReturn)
+  | For _ -> type_error s "typecheck_stmt: invalid for loop"
+  | While (exp,b) -> 
+    if (typecheck_exp tc exp) <> TBool then
+      type_error s "typecheck_stmt: while loop exp not a boolean"
+    else
+      let _ = typecheck_block b tc to_ret NoReturn in
+      (tc, NoReturn)
   end
 and typecheck_block (fbody:Ast.block) (tc:Tctxt.t) (ret_ty:Ast.ret_ty) (s_ty:stmt_type): Tctxt.t * stmt_type = 
   begin match fbody with
@@ -210,17 +314,19 @@ and typecheck_rty (l : 'a Ast.node) (tc : Tctxt.t) (rt : Ast.rty) : unit =
     let fields = lookup_struct_option id tc in
       begin match fields with
       | None -> type_error l (String.concat " " ["tyepcheck_rty: ctxt does not contain"; id]) 
-      | Some fs -> 
+      | Some fs -> () 
+                     (*
         let typecheck_rty_helper (f:Ast.field) : unit =  
           begin match f.ftyp with
           | TRef (RStruct id') -> 
-            if id != id' then 
+            if id <> id' then 
               typecheck_ty l tc f.ftyp
             else 
               ()
           | _ -> typecheck_ty l tc f.ftyp
           end in
         List.iter typecheck_rty_helper fs
+                      *)
       end
   | RArray ty -> typecheck_ty l tc ty
   | RFun fty -> typecheck_fty l tc fty
@@ -323,8 +429,11 @@ let create_function_ctxt (tc:Tctxt.t) (p:Ast.prog) : Tctxt.t =
 let create_global_ctxt (tc:Tctxt.t) (p:Ast.prog) : Tctxt.t =
   let helper (acc:t) (decl:Ast.decl) : t =
     begin match decl with
-    | Gvdecl gnode -> 
-      add_global acc gnode.elt.name (typecheck_exp acc gnode.elt.init)
+    | Gvdecl gnode ->
+       if lookup_global_option gnode.elt.name acc <> None then
+         type_error gnode "create_global_ctxt: duplicate global vars"
+       else
+         add_global acc gnode.elt.name (typecheck_exp tc gnode.elt.init)
     | _ -> acc
     end in
   List.fold_left helper tc p 
